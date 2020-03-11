b35eeea39f3742dfd14a86b96c5a5d0df9700d6a
theonekik <50080215+theonekik@users.noreply.github.com>
2020-03-12 01:21:49 +0530

    Just General sync

---
+++ if_elif_example.py
@@ -0,0 +1,12 @@
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+        print('You are not Alice, Kiddo.')
+elif age > 2000:
+                print('Unlike you , Alice not an undead, immortal vampire')
+elif age > 100:
+        print ('You are not Alice, greannie.')
+else:
+        print('You are not Alice')
---
+++ if_else_example.py
@@ -0,0 +1,5 @@
+password = 'Swordfish'
+if password == 'Swordfish':
+    print('Hi, Alice')
+else:
+    print('Wrong Password')
